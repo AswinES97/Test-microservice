b43ffbfce7f92e3f36c61374dd3216a269925a7b
aswin <aswinedassery@gmail.com>
2023-10-04 04:07:23 +0530

    profile cicd testing

--- auth/index.js
+++ auth/index.js
@@ -1,9 +1,5 @@
 const app = require('express')()
 
-app.get((req,res)=>{
-    res.send('hi')
-})
-
 app.listen(3000,()=>{
     console.log("auth listening on port 3000")
 })
--- profile/index.js
+++ profile/index.js
@@ -1,5 +1,9 @@
 const app = require('express')()
 
+app.get((req,res)=>{
+    res.send('hi')
+})
+
 app.listen(3001,()=>{
     console.log("profile listening on port 3001");
 })
