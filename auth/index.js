ae2f2cceddbc50c0829b82065fc76c3328ebb97e
aswin <aswinedassery@gmail.com>
2023-10-05 21:15:02 +0530

    producer added

--- auth/index.js
+++ auth/index.js
@@ -1,9 +1,22 @@
-const app = require('express')()
+const express = require('express')
+const app = express()
+const getProducer = require('./producer/index-proudcer')
 
-app.get((req,res)=>{
+app.use(express.json())
+
+app.post('/', async (req, res) => {
+    const { name } = req.body
+    const producer = await getProducer()
+
+    await producer.send({
+        topic: "username",
+        messages: [{ value: Buffer.from(name) }]
+    })
+
+    
     res.send('auth')
 })
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("auth listening on port 3000")
 })
